d2a0dcae61b0ca162b6383189ab3d8ed1cc9d2a2
田中　琉聖 <ryusei_tanaka@a7dsc215.local>
2025-06-02 11:35:41 +0900

    setteing

---
+++ src/main/java/searchman/Spring06ShainListAppApplication.java
@@ -0,0 +1,13 @@
+package searchman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06ShainListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring06ShainListAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/searchman/Spring06ShainListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package searchman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring06ShainListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
